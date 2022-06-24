e7607bd0fe215edc00abbe0e95bc09c2b5d5ef3e
brianbancroft <hello@brianbancroft.ca>
2022-06-24 11:23:45 -0700

    Basic app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,13 @@
 import { useState } from "react";
 import logo from "./logo.svg";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <header className="py-4 px-8 bg-slate-400">Connect four</header>
+      <main className="p-4"> hello world</main>
+    </>
   );
 }
 
