1061fe96d2b5b68d9f137d12ac2d1e43040f82e1
brianbancroft <hello@brianbancroft.ca>
2022-06-24 11:28:33 -0700

    Create crude spash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./index.css";
 
 function App() {
+  const [gameActive, setGameActive] = useState(false);
+
+  const startGame = () => setGameActive(true);
+
+  const SplashScreen = () => {
+    return (
+      <section>
+        <h2 className="text-2xl">Press button to start game</h2>
+        <button onClick={startGame}>click me</button>
+      </section>
+    );
+  };
+
+  const ActiveGame = () => {
+    return <section className="">game is active</section>;
+  };
+
   return (
     <>
       <header className="py-4 px-8 bg-slate-400">Connect four</header>
-      <main className="p-4"> hello world</main>
+      <main className="p-4">
+        {gameActive ? <ActiveGame /> : <SplashScreen />}
+      </main>
     </>
   );
 }
