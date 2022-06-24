b5290ba57c39e036c49a8cbec8df35fa0f431507
brianbancroft <hello@brianbancroft.ca>
2022-06-24 11:56:23 -0700

    Add some styling to pieces

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,27 +5,32 @@ import PropTypes from "prop-types";
 function Board(props) {
   const { boardStatus } = props;
 
-  const BlueToken = () => <div className="rounded-full bg-blue-600 w-12 h-12" />;
+  const BlueToken = () => (
+    <div className="rounded-full bg-blue-600 w-12 h-12" />
+  );
   const RedToken = () => <div className="rounded-full bg-red-600 w-12 h-12" />;
-  
-
 
   const boardSpaces = boardStatus.flat().map((token, index) => {
-    if (token === 'blue') return (
-        <div className="bg-white p-2 h-full" key={index}><BlueToken /></div>
-    )
+    if (token === "blue")
+      return (
+        <div className="bg-white p-2 h-full flex justify-center" key={index}>
+          <BlueToken />
+        </div>
+      );
 
-    if (token === 'red') return (
-        <div className="bg-white p-2 h-full" key={index}><RedToken /></div>
-    )
+    if (token === "red")
+      return (
+        <div className="bg-white p-2 h-full flex justify-center" key={index}>
+          <RedToken />
+        </div>
+      );
 
-    return <div className="bg-white p-2 h-full"  key={index}/>
-
-  })
+    return <div className="bg-white p-2 h-14" key={index} />;
+  });
 
   return (
     <section className="bg-slate-200 p-4 grid grid-cols-7 grid-rows-6 gap-4 items-center justify-center">
-        {boardSpaces}    
+      {boardSpaces}
     </section>
   );
 }
