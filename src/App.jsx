2bbcadfe92d30b7345940aad6f28af5ed5cf7294
brianbancroft <hello@brianbancroft.ca>
2022-06-24 17:41:23 -0700

    Update splash page

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,21 @@ function App() {
 
   const SplashScreen = () => {
     return (
-      <section>
-        <h2 className="text-2xl">Press button to start game</h2>
-        <div className="h-72 w-full flex items-center justify-center">
-          <button
-            onClick={startGame}
-            className="bg-blue-700 text-white text-2xl px-4 py-2 uppercase rounded"
-          >
-            click me
-          </button>
+      <section className="w-full h-full pt-8 pattern flex justify-center">
+        <div className="bg-white w-3/4 h-2/3 rounded border border-black shadow-xl p-8">
+          <h2 className="text-2xl my-3">A react-based sample</h2>
+          <p className="my-2">
+            Connect three plus one is based on the game of a similar name. A
+            sample demo.
+          </p>
+          <div className="h-72 w-full flex items-center justify-center">
+            <button
+              onClick={startGame}
+              className="bg-blue-400 hover:bg-blue-500 active:bg-blue-700 border-b-4 active:border-b-0 border-blue-700 transition-all ease-in-out drop-shadow-xl text-white text-2xl px-4 py-2 uppercase rounded"
+            >
+              start game
+            </button>
+          </div>
         </div>
       </section>
     );
@@ -27,8 +33,10 @@ function App() {
 
   return (
     <>
-      <header className="py-4 px-8 bg-slate-400">Connect four</header>
-      <main className="p-4">
+      <header className="py-4 px-8 bg-slate-400 border-b-8 ">
+        Connect three plus one
+      </header>
+      <main className="w-screen h-screen flex justify-center">
         {gameActive ? <ActiveGame onGameEnd={endGame} /> : <SplashScreen />}
       </main>
     </>
