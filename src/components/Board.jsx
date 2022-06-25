eaecf6ed21f168afd47409b79e4307818c467815
brianbancroft <hello@brianbancroft.ca>
2022-06-25 08:55:59 -0700

    Add feet and semantic names to board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,12 +14,19 @@ function Board(props) {
 
   return (
     <>
-      <section className="bg-yellow-200 p-4 grid grid-cols-7 grid-rows-6 gap-4 items-center justify-center border border-black">
+      <section
+        className="bg-yellow-200 py-4 px-2 grid grid-cols-7 grid-rows-6 gap-4 items-center justify-items-center border border-black"
+        id="gameBoard"
+      >
         {boardSpaces}
       </section>
       <div className="flex justify-between">
-        <div className="bg-yellow-200 w-10 h-6 border-b border-x border-black ml-8"></div>
-        <div className="bg-yellow-200 w-10 h-6 border-b border-x border-black mr-8"></div>
+        <div className="bg-yellow-200 w-10 h-6 border-b-4 border-b-gray-500 border-x border-black ml-8">
+          <div className="hidden">left foot</div>
+        </div>
+        <div className="bg-yellow-200 w-10 h-6 border-b-4 border-b-gray-500 border-x border-black mr-8">
+          <div className="hidden">right foot</div>
+        </div>
       </div>
     </>
   );
