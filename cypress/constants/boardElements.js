45c9f24bf86ef331767f84c75ad833228e8b759b
brianbancroft <hello@brianbancroft.ca>
2022-06-27 09:57:59 -0700

    Add contants file

---
+++ cypress/constants/boardElements.js
@@ -0,0 +1,17 @@
+export default {
+  inactiveSlotName: "no-token",
+  numberSlots: 42,
+  cssInactiveColor: "#000000",
+  player1: {
+    name: "Player 1",
+    activeColor: "rgb(37, 99, 235)",
+    tailwindColor: "blue",
+    tokenLabel: "token-blue",
+  },
+  player2: {
+    name: "Player 2",
+    activeColor: "rgb(220, 38, 38)",
+    tailwindColor: "red",
+    tokenLabel: "token-red",
+  },
+};
